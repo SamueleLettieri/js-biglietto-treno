006a4460c0fc10a9c9fa3f5c2f0aa4bd6a076c10
SamueleLettieri <samuele2073@gmail.com>
2022-06-14 18:39:59 +0200

    programma x per calcolare il prezzo del biglietto in pag

--- js/myscript.js
+++ js/myscript.js
@@ -7,17 +7,18 @@ console.log(yourAge);
 const kmPrice = 0.21;
 
 const normalRunning = kmToGo * kmPrice ;
+console.log(normalRunning)
 
 
    if ( (isNaN(yourAge) == true)) {
     console.log("numero non trovato")
 } else {
     if (yourAge < 18){
-        console.log(Math.round( normalRunning - normalRunning * 20 / 100 ) ) /100;
+        document.getElementById("ticket-price").innerHTML = Math.round(normalRunning - normalRunning * 20 / 100);
     } else if (yourAge > 65) {
-        console.log(Math.round( normalRunning - normalRunning * 40 / 100) ) /100;
+        document.getElementById("ticket-price").innerHTML = Math.round(normalRunning - normalRunning * 40 / 100);
     } else {
-        console.log(Math.round( normalRunning) ) /100;
+        document.getElementById("ticket-price").innerHTML = Math.round(normalRunning);
     }
 } 
 
